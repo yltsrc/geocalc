32ef60a2aa739ebb612449fd8b93e3c99021c319
Yura Tolstik <yltsrc@gmail.com>
2016-09-14 19:36:57 +0300

    version 0.5.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Geocalc.Mixfile do
   def project do
     [app: :geocalc,
      name: "Geocalc",
-     version: "0.5.0",
+     version: "0.5.1",
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
