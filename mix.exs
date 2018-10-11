051f89cbc3add164b9f4660e7acba76fb6eab819
Yura Tolstik <yltsrc@gmail.com>
2018-10-11 12:27:33 +0300

    add credo to travis build

--- mix.exs
+++ mix.exs
@@ -37,7 +37,7 @@ defmodule Geocalc.Mixfile do
     [
       {:benchfella, "~> 0.3.5", only: :bench},
       {:coverex, "~> 1.5.0", only: :test},
-      {:credo, "~> 0.10.2", only: :dev},
+      {:credo, "~> 0.10.2", only: [:dev, :test]},
       {:ex_doc, "~> 0.19.1", only: :dev},
       {:mix_test_watch, "~> 0.9.0", only: :dev}
     ]
